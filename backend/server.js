87f235e22746738cf520c393b9160be30b91baed
Chirag205444 <chiragshetty9204@gmail.com>
2026-05-06 22:07:55 +0530

    deployment fix

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ const recipeRoutes = require('./routes/recipeRoute');
 const userRoutes = require('./routes/userRoute');
 
 // ✅ CORS (temporary for now)
-app.use(cors());
+app.use(cors({
+  origin: "food-stack-mern-alpha.vercel.app"
+}));
 
 // ✅ Middlewares
 app.use(express.json());
